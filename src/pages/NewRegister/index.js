552cd2e0723beac3d6d36751e5857cbc9156ca11
anderson-lda <anderson.lima.araujo@hotmail.com>
2025-02-06 21:41:37 -0300

    registrando fluxo de caixa

--- src/pages/NewRegister/index.js
+++ src/pages/NewRegister/index.js
@@ -1,13 +1,56 @@
-import { Keyboard, SafeAreaView, TouchableWithoutFeedback } from "react-native";
+import { Alert, Keyboard, SafeAreaView, TouchableWithoutFeedback } from "react-native";
 import Header from "../../components/Header";
 import { Background, Input, SubmitButton, SubmitText } from "./styles";
 import { useState } from "react";
 import RegisterTypes from "../../components/RegisterTypes";
+import api from "../../services/api";
+import { format } from "date-fns";
+import { useNavigation } from "@react-navigation/native";
 
 export default function NewRegister(){
+    const navigation = useNavigation()
     const [labelInput, setLabelInput] = useState('')
     const [valueInput, setValueInput] = useState('')
     const [type, setType] = useState('receita')
+
+    function handleSubmit(){
+        Keyboard.dismiss()
+
+        if(isNaN(parseFloat(valueInput)) || type === null){
+            alert('Preencha todos os campos')
+            return
+        }
+
+        Alert.alert(
+            'Confirmando dados'
+            `Tipo: ${type} - Valor: ${parse.float(valueInput)}`,
+            [
+                {
+                    text: 'Cancelar',
+                    style: 'cancel'
+                },
+                {
+                    text: 'Continuar',
+                    onPress: () => handleAdd()
+                }
+            ]
+        )
+    }
+
+    async function handleAdd(){
+        Keyboard.dismiss()
+
+        await api.post('/receive', {
+            description: labelInput,
+            value: Number(valueInput),
+            type: type,
+            date: format(new Date(), 'dd/MM/yyyy')
+        }) //token jávai automaticamente, configurado pelo auth
+
+        setLabelInput('')
+        setValueInput('')
+        navigation.navigate('Home')
+    }
 
     return (
         <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
@@ -23,7 +66,7 @@ export default function NewRegister(){
                         onChangeText={(text)=>setValueInput(text)}
                     />
                     <RegisterTypes type={type} setTypeChanged={(item) => setType(item)} />
-                    <SubmitButton>
+                    <SubmitButton onPress={handleSubmit}>
                         <SubmitText>Registrar</SubmitText>
                     </SubmitButton>
                 </SafeAreaView>
