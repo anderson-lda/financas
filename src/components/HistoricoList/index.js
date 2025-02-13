bb20cc6a2052fc0f52e5d99dca5ec002e729b6d0
anderson-lda <anderson.lima.araujo@hotmail.com>
2025-02-13 10:10:44 -0300

    deletando registros

--- src/components/HistoricoList/index.js
+++ src/components/HistoricoList/index.js
@@ -1,23 +1,43 @@
+import { Alert, TouchableWithoutFeedback } from "react-native";
 import { Container, TipoText } from "./styles";
 import Icon from 'react-native-vector-icons/Feather'
 
-export default function HistoricoList({ data }){
-    return (
-        <Container>
-            <Tipo>
-                <IconView tipo={data.type}>
-                    <Icon 
-                        name={data.type === 'despesa' ? 'arrow-down' : 'arrow-up'} 
-                        size={20} 
-                        color="#FFF" 
-                    />
-                    <TipoText>{data.type}</TipoText>
-                </IconView>
-            </Tipo>
+export default function HistoricoList({ data, deleteItem }){
+    function handleDeleteItem(){
+        Alert.alert(
+            'Atenção',
+            'VOcê tem certeza da deleção deste registro?',
+            [
+                {
+                    text: 'Cancelar',
+                    style: 'cancel'
+                },
+                {
+                    text: 'Continuar',
+                    onPress: () => deleteItem(data.id)
+                }
+            ]
+        )
+    }
 
-            <Valortext>
-                R$ {data.value}
-            </Valortext>
-        </Container>
+    return (
+        <TouchableWithoutFeedback onLongPress={handleDeleteItem}>
+            <Container>
+                <Tipo>
+                    <IconView tipo={data.type}>
+                        <Icon 
+                            name={data.type === 'despesa' ? 'arrow-down' : 'arrow-up'} 
+                            size={20} 
+                            color="#FFF" 
+                        />
+                        <TipoText>{data.type}</TipoText>
+                    </IconView>
+                </Tipo>
+
+                <Valortext>
+                    R$ {data.value}
+                </Valortext>
+            </Container>
+        </TouchableWithoutFeedback>
     )
 }
